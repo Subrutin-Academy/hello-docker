314fd0ac6fbea7f69c9cbe1bbdd905adc759cb8a
Tedy Tri Saputro <tedy@saputro.dev>
2023-08-20 10:53:17 +0700

    [ADD} add lombok, and log

--- src/main/java/com/subrutin/docker/web/HelloResource.java
+++ src/main/java/com/subrutin/docker/web/HelloResource.java
@@ -1,17 +1,26 @@
 package com.subrutin.docker.web;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.subrutin.docker.dto.GreetingDTO;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HelloResource {
 	
 	@GetMapping("/v1/hello")
-	public ResponseEntity<GreetingDTO> greeting(){
-		return ResponseEntity.ok().body(new GreetingDTO("Hello World"));
+	public ResponseEntity<GreetingDTO> greeting() throws UnknownHostException{
+		log.info("request served");
+		InetAddress ip=InetAddress.getLocalHost();
+		return ResponseEntity.ok().body(new GreetingDTO("Hello World. This is from "+ip.toString()));
 	}
 
 }
